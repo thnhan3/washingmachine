a42ec61b30dade225bfdcbdbfbdf4a973d99f38e
thnhan3 <huunhantran10@gmail.com>
2024-09-19 21:58:08 +0700

    update env config

--- supabase.js
+++ supabase.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 const supabaseUrl = "https://eoxllivvvonazpnpwugr.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
-console.log(process.env.SUPABASE_KEY);
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
